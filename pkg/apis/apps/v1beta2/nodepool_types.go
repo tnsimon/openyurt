39c82bc168d9e335886a8c49865f6f234f0fca6d
Simon Tien <tiensimon@microsoft.com>
2025-01-10 14:15:36 +1100

    feat: upgrade nodepool crd to v1beta2

---
+++ pkg/apis/apps/v1beta2/nodepool_types.go
@@ -0,0 +1,164 @@
+/*
+Copyright 2024 The OpenYurt Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta2
+
+import (
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type NodePoolType string
+
+// LeaderElectionStrategy represents the policy how to elect a leader Yurthub in a nodepool.
+type LeaderElectionStrategy string
+
+const (
+	Edge  NodePoolType = "Edge"
+	Cloud NodePoolType = "Cloud"
+
+	ElectionStrategyMark   LeaderElectionStrategy = "mark"
+	ElectionStrategyRandom LeaderElectionStrategy = "random"
+
+	// LeaderStatus means the status of leader yurthub election.
+	// If it's ready the leader elected, otherwise no leader is elected.
+	LeaderStatus NodePoolConditionType = "LeaderReady"
+)
+
+// NodePoolSpec defines the desired state of NodePool
+type NodePoolSpec struct {
+	// The type of the NodePool
+	// +optional
+	Type NodePoolType `json:"type,omitempty"`
+
+	// HostNetwork is used to specify that cni components(like flannel)
+	// will not be installed on the nodes of this NodePool.
+	// This means all pods on the nodes of this NodePool will use
+	// HostNetwork and share network namespace with host machine.
+	HostNetwork bool `json:"hostNetwork,omitempty"`
+
+	// If specified, the Labels will be added to all nodes.
+	// NOTE: existing labels with samy keys on the nodes will be overwritten.
+	// +optional
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// If specified, the Annotations will be added to all nodes.
+	// NOTE: existing labels with samy keys on the nodes will be overwritten.
+	// +optional
+	Annotations map[string]string `json:"annotations,omitempty"`
+
+	// If specified, the Taints will be added to all nodes.
+	// +optional
+	Taints []v1.Taint `json:"taints,omitempty"`
+
+	// InterConnectivity represents all nodes in the NodePool can access with each other
+	// through Layer 2 or Layer 3 network or not. If the field is true,
+	// nodepool-level list/watch requests reuse can be applied for this nodepool.
+	// otherwise, only node-level list/watch requests reuse can be applied for the nodepool.
+	// This field cannot be changed after creation.
+	InterConnectivity bool `json:"interConnectivity,omitempty"`
+
+	// LeaderElectionStrategy represents the policy how to elect a leader Yurthub in a nodepool.
+	// random: select one ready node as leader at random.
+	// mark: select one ready node as leader from nodes that are specified by labelselector.
+	// More strategies will be supported according to user's new requirements.
+	LeaderElectionStrategy string `json:"leaderElectionStrategy,omitempty"`
+
+	// LeaderNodeLabelSelector is used only when LeaderElectionStrategy is mark. leader Yurhub will be
+	// elected from nodes that filtered by this label selector.
+	LeaderNodeLabelSelector map[string]string `json:"leaderNodeLabelSelector,omitempty"`
+
+	// PoolScopeMetadata is used for specifying resources which will be shared in the nodepool.
+	// And it is supported to modify dynamically. and the default value is v1.Service and discovery.Endpointslice.
+	PoolScopeMetadata []metav1.GroupVersionKind `json:"poolScopeMetadata,omitempty"`
+}
+
+// NodePoolStatus defines the observed state of NodePool
+type NodePoolStatus struct {
+	// Total number of ready nodes in the pool.
+	// +optional
+	ReadyNodeNum int32 `json:"readyNodeNum"`
+
+	// Total number of unready nodes in the pool.
+	// +optional
+	UnreadyNodeNum int32 `json:"unreadyNodeNum"`
+
+	// The list of nodes' names in the pool
+	// +optional
+	Nodes []string `json:"nodes,omitempty"`
+
+	// LeaderEndpoints is used for storing the address of Leader Yurthub.
+	// +optional
+	LeaderEndpoints []string `json:"leaderEndpoints,omitempty"`
+
+	// Conditions represents the latest available observations of a NodePool's
+	// current state that includes LeaderHubElection status.
+	// +optional
+	Conditions []NodePoolCondition `json:"conditions,omitempty"`
+}
+
+// NodePoolConditionType represents a NodePool condition value.
+type NodePoolConditionType string
+
+type NodePoolCondition struct {
+	// Type of NodePool condition.
+	Type NodePoolConditionType `json:"type,omitempty"`
+
+	// Status of the condition, one of True, False, Unknown.
+	Status v1.ConditionStatus `json:"status,omitempty"`
+
+	// Last time the condition transitioned from one status to another.
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
+
+	// The reason for the condition's last transition.
+	Reason string `json:"reason,omitempty"`
+
+	// A human readable message indicating details about the transition.
+	Message string `json:"message,omitempty"`
+}
+
+// +genclient
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:scope=Cluster,path=nodepools,shortName=np,categories=all
+// +kubebuilder:printcolumn:name="Type",type="string",JSONPath=".spec.type",description="The type of nodepool"
+// +kubebuilder:printcolumn:name="ReadyNodes",type="integer",JSONPath=".status.readyNodeNum",description="The number of ready nodes in the pool"
+// +kubebuilder:printcolumn:name="NotReadyNodes",type="integer",JSONPath=".status.unreadyNodeNum"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:subresource:status
+// +genclient:nonNamespaced
+// +kubebuilder:storageversion
+
+// NodePool is the Schema for the nodepools API
+type NodePool struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   NodePoolSpec   `json:"spec,omitempty"`
+	Status NodePoolStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// NodePoolList contains a list of NodePool
+type NodePoolList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []NodePool `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&NodePool{}, &NodePoolList{})
+}
---
+++ pkg/yurtmanager/webhook/nodepool/v1beta2/nodepool_default.go
@@ -0,0 +1,81 @@
+/*
+Copyright 2023 The OpenYurt Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta2
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/openyurtio/openyurt/pkg/apis/apps"
+	"github.com/openyurtio/openyurt/pkg/apis/apps/v1beta2"
+)
+
+// Default satisfies the defaulting webhook interface.
+func (webhook *NodePoolHandler) Default(ctx context.Context, obj runtime.Object) error {
+	np, ok := obj.(*v1beta2.NodePool)
+	if !ok {
+		return apierrors.NewBadRequest(fmt.Sprintf("expected a NodePool but got a %T", obj))
+	}
+
+	// specify default type as Edge
+	if len(np.Spec.Type) == 0 {
+		np.Spec.Type = v1beta2.Edge
+	}
+
+	if np.Labels == nil {
+		np.Labels = map[string]string{
+			apps.NodePoolTypeLabel: strings.ToLower(string(np.Spec.Type)),
+		}
+	} else {
+		np.Labels[apps.NodePoolTypeLabel] = strings.ToLower(string(np.Spec.Type))
+	}
+
+	// init node pool status
+	np.Status = v1beta2.NodePoolStatus{
+		ReadyNodeNum:   0,
+		UnreadyNodeNum: 0,
+		Nodes:          make([]string, 0),
+	}
+
+	// Set default election strategy
+	if np.Spec.LeaderElectionStrategy == "" {
+		np.Spec.LeaderElectionStrategy = string(v1beta2.ElectionStrategyRandom)
+	}
+
+	// Set default PoolScopeMetadata
+	if np.Spec.PoolScopeMetadata == nil {
+		np.Spec.PoolScopeMetadata = []v1.GroupVersionKind{
+			{
+				Group:   "core",
+				Version: "v1",
+				Kind:    "Service",
+			},
+			{
+				Group:   "discovery.k8s.io",
+				Version: "v1",
+				Kind:    "EndpointSlice",
+			},
+		}
+	}
+
+	return nil
+}
